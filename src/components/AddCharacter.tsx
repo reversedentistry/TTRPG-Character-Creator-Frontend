b230d3da638e52771d25f350f919da1d7633bf61
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-16 21:21:21 -0700

    Filled in more of axios method

--- src/components/AddCharacter.tsx
+++ src/components/AddCharacter.tsx
@@ -1,14 +1,37 @@
 import { Formik } from "formik";
-import axios from "axios"; 
+import axios, { AxiosRequestConfig } from "axios"; 
 
 function addCharacter() {
-    const newCharacterRequest = axios.post("http://localhost:5000/pcs", {}); 
+    const newCharacterRequest = async (playerCharacter: {name: string, level: number, hitPoints: number, strength: number, intelligence: number, dexterity: number, wisdom: number, charisma: number, constitution: number}) => {
+        const requestConfig: AxiosRequestConfig = {
+            headers: {
+                "name": playerCharacter.name,
+                "level": playerCharacter.level,
+                "hitPoints": playerCharacter.hitPoints,
+                "strength": playerCharacter.strength,
+                "intelligence": playerCharacter.intelligence,
+                "dexterity": playerCharacter.dexterity,
+                "wisdom": playerCharacter.wisdom,
+                "charisma": playerCharacter.charisma,
+                "constitution": playerCharacter.constitution,
+                "Content-Type": "application/json"
+            }
+        }
+        
+        axios.post("http://localhost:5000/pcs", requestConfig)
+            .then((res) => {
+
+            }
+        ).catch(); 
+    }
     
     return 
     <div>
         <Formik
             initialValues={{ name: "", level: "", hitPoints: "", strength: "", intelligence: "", dexterity: "", wisdom: "", charisma: "", constitution: ""}}
-            onSubmit={(values) => {}}>
+            onSubmit={(values) => {
+                newCharacterRequest
+            }}>
 
         </Formik>
     </div>
