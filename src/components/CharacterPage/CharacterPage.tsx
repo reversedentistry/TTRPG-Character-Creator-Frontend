2229a8b422a4fb2457fd6adeec90625cffb63bff
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-07-05 09:17:05 -0700

    Added buttons and deletion functionality

--- src/components/CharacterPage/CharacterPage.tsx
+++ src/components/CharacterPage/CharacterPage.tsx
@@ -1,38 +1,57 @@
 import axios from "axios";
 import { CharacterInfo } from "../../models/CharacterInfo";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function CharacterPage() {
     const [characterInfo, setCharacterInfo] = useState<CharacterInfo>();
-    
+
     let { characterId } = useParams();
+
+    const navigate = useNavigate();
 
     const retrieveCharacterInfo = () => {
         axios.get(`http://localhost:5000/pcs/${characterId}`)
+            .then(res => {
+                setCharacterInfo(res.data);
+            }).catch(e => {
+                console.log(e);
+            });
+    }
+
+    const openCharacterEditor = () => {
+
+    }
+    
+    const deleteCharacter = () => {
+        axios.delete(`http://localhost:5000/pcs/${characterId}`)
         .then(res => {
-            setCharacterInfo(res.data);
+            navigate("/characters");
         }).catch(e => {
-            console.log(e); 
-        }); 
+            console.log(e);
+        });
     }
 
     useEffect(() => {
-        retrieveCharacterInfo(); 
+        retrieveCharacterInfo();
     }, [characterInfo]);
 
     return (
-        <div>
-            <p>{characterInfo?.pcName}</p>
-            <p>Level: {characterInfo?.level}</p>
-            <p>Maximum hitpoints: {characterInfo?.hitPoints}</p>
-            <p>Strength: {characterInfo?.strength}</p>
-            <p>Intelligence: {characterInfo?.intelligence}</p>
-            <p>Dexterity: {characterInfo?.dexterity}</p>
-            <p>Wisdom: {characterInfo?.wisdom}</p>
-            <p>Charisma: {characterInfo?.charisma}</p>
-            <p>Constitution: {characterInfo?.constitution}</p>
-        </div>
+        <section>
+            <button onClick={openCharacterEditor}>Edit character</button>
+            <div>
+                <p>{characterInfo?.pcName}</p>
+                <p>Level: {characterInfo?.level}</p>
+                <p>Maximum hitpoints: {characterInfo?.hitPoints}</p>
+                <p>Strength: {characterInfo?.strength}</p>
+                <p>Intelligence: {characterInfo?.intelligence}</p>
+                <p>Dexterity: {characterInfo?.dexterity}</p>
+                <p>Wisdom: {characterInfo?.wisdom}</p>
+                <p>Charisma: {characterInfo?.charisma}</p>
+                <p>Constitution: {characterInfo?.constitution}</p>
+            </div>
+            <button onClick={deleteCharacter}>Delete character</button>
+        </section>
     )
 }
 
