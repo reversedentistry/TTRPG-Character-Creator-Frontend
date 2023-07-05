b4d3d499a9a5a9b31d59cb4b26b58a09d73b4b20
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-07-05 09:16:39 -0700

    Added path to character list page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AddCharacter from './components/AddCharacter/AddCharacter';
 import './App.css';
 import CharacterPage from './components/CharacterPage/CharacterPage';
+import CharacterList from './components/CharacterList/CharacterList';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/characters" element={<CharacterList/>}/>
         <Route path="/" element={<AddCharacter/>}/>
         <Route path="/:characterId" element={<CharacterPage/>}/>
       </Routes>
