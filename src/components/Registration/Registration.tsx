9a091c7c01e8279d34f1c20e2468096530b48158
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-07-17 14:09:30 -0700

    Created form for player registration

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,17 +1,41 @@
 import { Formik, Form, Field } from "formik";
 import axios from "axios";
-import { PlayerInfo } from "../../models/PlayerInfo";
+
 
 function Registration() {
-    const newPlayerInitialValues:PlayerInfo = {
+    interface RegistrationInfo {
+        username:string,
+        playerName:string,
+        password:string
+    }
+    
+    const newPlayerInitialValues:RegistrationInfo = {
         username: "",
+        playerName: "",
         password: ""
     }    
     
-    const createNewUser = async () => {};
+    const createNewUser = async (newPlayer:RegistrationInfo) => {};
     
     return (
-        <div><Formik initialValues={newPlayerInitialValues} onSubmit={createNewUser}></Formik></div>
+        <div>
+            <Formik initialValues={newPlayerInitialValues} onSubmit={createNewUser}>
+                {({ values }) => (
+                    <Form>
+                        <label>Enter a username to use for login</label>
+                        <Field id="username" name="username" value={values.username} placeholder="Username"></Field>
+
+                        <label>Enter a name</label>
+                        <Field id="playerName" name="playerName" value={values.playerName} placeholder="Name"></Field>
+
+                        <label>Password</label>
+                        <Field id="password" name="password" value={values.password} type="password" placeholder="Password"></Field>
+
+                        <button type="submit">Submit</button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
         
     )
 }
